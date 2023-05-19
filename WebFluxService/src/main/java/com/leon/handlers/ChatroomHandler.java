8f41c07c9ccb770cc779a64f09826459f39d4825
LeonAdeoye <leon.adeoye@gmail.com>
2023-05-19 17:27:35 +0800

    Added more dependencies

--- WebFluxService/src/main/java/com/leon/handlers/ChatroomHandler.java
+++ WebFluxService/src/main/java/com/leon/handlers/ChatroomHandler.java
@@ -22,6 +22,6 @@ public class ChatroomHandler
 	public Mono<ServerResponse> getAllRooms(ServerRequest request)
 	{
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-				.body(BodyInserters.fromPublisher(Flux.just(4,5,6).log(), Integer.class));
+				.body(BodyInserters.fromPublisher(Flux.interval(Duration.ofMillis(500)).take(10).log(), Long.class));
 	}
 }
