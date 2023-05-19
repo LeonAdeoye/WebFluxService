391b824a97802d69a2eb903fead64c7af540c921
LeonAdeoye <leon.adeoye@gmail.com>
2023-05-19 16:34:01 +0800

    first commit

--- WebFluxService/src/main/java/com/leon/handlers/ChatroomHandler.java
+++ WebFluxService/src/main/java/com/leon/handlers/ChatroomHandler.java
@@ -8,18 +8,20 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Component
 public class ChatroomHandler
 {
 	public Mono<ServerResponse> getAllUsers(ServerRequest request)
 	{
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-				.body(BodyInserters.fromPublisher(Flux.just(1,2,3), Integer.class));
+				.body(BodyInserters.fromPublisher(Flux.just(0,1,2,3).delayElements(Duration.ofSeconds(1)).log(), Integer.class));
 	}
 
 	public Mono<ServerResponse> getAllRooms(ServerRequest request)
 	{
 		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
-				.body(BodyInserters.fromPublisher(Flux.just(4,5,6), Integer.class));
+				.body(BodyInserters.fromPublisher(Flux.just(4,5,6).log(), Integer.class));
 	}
 }
